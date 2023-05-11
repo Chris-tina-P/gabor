4295aa76d6009b005cd760e34ee8651fffbec5bf
Chris-tina-P <72312097+Chris-tina-P@users.noreply.github.com>
2023-05-11 15:00:51 +0200

    Adding initial files

---
+++ code/main.py
@@ -0,0 +1,38 @@
+from scipy.io import wavfile
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.fftpack import fft, fftshift
+
+
+class Gabor:
+
+    def __init__(self):
+        self.data = None
+
+    def read_wav(self, name):
+        # read in sound file
+        samplerate, self.data = wavfile.read(name)
+
+        # define sound metadata
+        data_size = self.data.shape[0]
+        song_length_seconds = data_size / samplerate
+
+        # print sound metadata
+        print("Data size:", data_size)
+        print("Sample rate:", samplerate)
+        print("Song length (seconds):", song_length_seconds, "seconds")
+
+    # This method plots the loaded sound file
+    def plot_sound(self):
+        # plot sound file
+        plt.plot(self.data)
+        plt.title("Sound file")
+        plt.xlabel("Time")
+        plt.ylabel("Amplitude")
+        plt.show()
+
+
+if __name__ == '__main__':
+    gabor = Gabor()
+    gabor.read_wav('input/file_example_WAV_1MG.wav')
+    gabor.plot_sound()
